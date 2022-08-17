c7647dfb3e7a575f17ee6c9bb1f96a02bd05fb3d
Tracy Mawanda <mawandatracy@gmail.com>
2022-08-17 12:13:05 +0300

    deploying to heroku

---
+++ Procfile
@@ -0,0 +1,4 @@
+init: python3 manage.py db init && python3 manage.py db migrate && python3 manage.py db upgrade
+upgrade: pip install -r requirements.txt && python3 manage.py db upgrade
+create_defaults: python3 manage.py create_defaults
+web: gunicorn wsgi:app
--- pouch/settings.py
+++ pouch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +129,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
